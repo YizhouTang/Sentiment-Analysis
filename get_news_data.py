e9499d46547985310eb0b228d1f44c5c7ee55313
YizhouTang <34849408+YizhouTang@users.noreply.github.com>
2021-01-10 09:08:39 -0500

    - Wrote script to get historical news data - Wrote script to perform sentiment analysis on historical news data - Added some plots

---
+++ get_news_data.py
@@ -0,0 +1,30 @@
+from newsapi import NewsApiClient #https://newsapi.org/docs/client-libraries/python
+import pandas as pd
+import math
+
+def getNews(api_key,keyword,begin_date,end_date):
+    newsapi = NewsApiClient(api_key=api_key)
+    all_articles = newsapi.get_everything(q=keyword,
+                                          sources='the-wall-street-journal',#,bloomberg',#,business-insider,fortune',
+                                          domains='wsj.com',#,bloomberg.com',#,businessinsider.com,fortune.com',
+                                          from_param=begin_date,
+                                          to=end_date,
+                                          language='en',
+                                          sort_by='publishedAt',
+                                          page=1)
+    articles = all_articles['articles']
+    numPages = math.ceil(all_articles['totalResults'] / 20)
+
+    if numPages > 1:
+           for i in range(2,min(5+1,numPages+1)):#currently only support 5 pages due to free account
+                  temp_articles = newsapi.get_everything(q=keyword,
+                                                 sources='the-wall-street-journal',#,bloomberg',#,business-insider,fortune',
+                                                 domains='wsj.com',#,bloomberg.com',#,businessinsider.com,fortune.com',
+                                                 from_param=begin_date,
+                                                 to=end_date,
+                                                 language='en',
+                                                 sort_by='publishedAt',
+                                                 page=i)
+                  articles.extend(temp_articles['articles'])
+    df = pd.DataFrame(articles)
+    return df
---
+++ sentiment_analysis.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from get_news_data import getNews
+from datetime import date,timedelta
+import dateutil.parser
+import numpy as np
+from matplotlib import pyplot as plt
+
+API_KEY = '8e2c29370d674ab1a4e154cb42636473'
+
+BEGIN_DATE = date.today()- timedelta(30)
+BEGIN = BEGIN_DATE.strftime('%Y-%m-%d')
+END = date.today().strftime('%Y-%m-%d')
+KEYWORD = "Biden"
+
+#nltk.download('vader_lexicon') #only run this once
+
+df = getNews(API_KEY,KEYWORD,BEGIN,END)
+df['count'] = 1
+
+analyzer  = SentimentIntensityAnalyzer()
+
+for i in range(len(df)):
+    titleSentiment = analyzer.polarity_scores(df['title'][i])['compound']
+    descriptionSentiment = analyzer.polarity_scores(df['description'][i])['compound']
+    df.loc[i,'sentiment'] = (titleSentiment + descriptionSentiment) / 2
+    df.loc[i,'datetime'] = dateutil.parser.parse(df.loc[i,'publishedAt']).strftime('%Y-%m-%d')
+
+
+
+df_daily = df.groupby('datetime').mean().loc[:,['sentiment']]
+df_daily['numNews'] = df.groupby('datetime').sum().loc[:,'count']
+
+df.to_csv('example_sentiment_scores/News Sentiment - ' + KEYWORD + '.csv')
+df_daily.to_csv('example_sentiment_scores/Daily Sentiment - ' + KEYWORD + '.csv')
+
+
+#Plot graph with 2 y axes
+fig, ax1 = plt.subplots()
+fig.set_size_inches(15,8)
+#Plot bars
+ax1.bar(df_daily.index, df_daily['numNews'], alpha=0.3)
+ax1.set_xlabel('$Date$')
+
+# Make the y-axis label and tick labels match the line color.
+ax1.set_ylabel('Number of Articles', color='b')
+[tl.set_color('b') for tl in ax1.get_yticklabels()]
+
+#Set up ax2 to be the second y axis with x shared
+ax2 = ax1.twinx()
+#Plot a line
+ax2.plot(df_daily.index, df_daily['sentiment'], 'r-')
+
+# Make the y-axis label and tick labels match the line color.
+ax2.set_ylabel('Sentiment', color='r')
+[tl.set_color('r') for tl in ax2.get_yticklabels()]
+fig.autofmt_xdate()
+ax1.set_title('Sentiment Analysis - '+ KEYWORD)
+
+plt.show()
+plt.savefig('example_plots/Sentiment Analysis - '+ KEYWORD + '.png')
